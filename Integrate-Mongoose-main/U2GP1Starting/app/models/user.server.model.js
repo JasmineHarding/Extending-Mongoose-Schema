4b5a52d0f7ebedf552d55231b4327a45b94800e0
JasmineHarding <jasmine.harding0927@gmail.com>
2023-11-19 02:33:02 -0500

    2.4

--- Integrate-Mongoose-main/U2GP1Starting/app/models/user.server.model.js
+++ Integrate-Mongoose-main/U2GP1Starting/app/models/user.server.model.js
@@ -5,8 +5,28 @@ const UserSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
-    password: String
+    username: {
+        type: String,
+        trim: true
+    },
+    password:String, 
+    created:{
+        type: Date, 
+        default: Date.now
+    }, 
+    website: {
+        type: String,
+        set: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !==0 && url.indexOf('https://') !== 0) 
+                    url = 'http://' + url;
+            }
+            return url;
+        }
+    }
 });
 
-mongoose.model('User', UserSchema);
+UserSchema.set('toJSON', { getters: true });
+mongoose.model('User', UserSchema)
